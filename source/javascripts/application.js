859f42ed21a15a5cedd42de77bec86a9bc2235f7
Vinh Dang <vinhdangphuc@gmail.com>
2017-05-06 09:39:46 +0700

    Update TechTalks UI add sidebar, add back to top button, navigation highlight

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,1 +1,42 @@
-// This is where it all goes :)
+$(function(){
+    // this will get the full URL at the address bar
+    var url = window.location.href; 
+    console.log(url);
+    // passes on every "a" tag 
+    $("#navbar-collapse a").each(function() {
+            // checks if its the same on the address bar
+            console.log(this.href);
+        if(url == (this.href)) { 
+            $(this).closest("li").addClass("active");
+        }
+    });
+});
+
+jQuery(document).ready(function($){
+	// browser window scroll (in pixels) after which the "back to top" link is shown
+	var offset = 300,
+		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		offset_opacity = 1200,
+		//duration of the top scrolling animation (in ms)
+		scroll_top_duration = 700,
+		//grab the "back to top" link
+		$back_to_top = $('.cd-top');
+
+	//hide or show the "back to top" link
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+		if( $(this).scrollTop() > offset_opacity ) { 
+			$back_to_top.addClass('cd-fade-out');
+		}
+	});
+
+	//smooth scroll to top
+	$back_to_top.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scroll_top_duration
+		);
+	});
+
+});
