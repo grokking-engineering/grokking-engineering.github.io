ab9669f72224384f029f3815e7081bf883057e5e
Huy Nguyen <nvquanghuy@gmail.com>
2018-06-17 11:54:44 +0700

    Test travis

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -6,13 +6,11 @@ git fetch --depth 1 origin
 git checkout gh-pages
 git fetch --depth 1 origin gh-pages
 git reset --hard origin/gh-pages
-mv /home/travis/build/grokking-vietnam/landing/build/.git /tmp/d20180617-15767-19vjt9c
+rm -rf /tmp/d20180617-15767-19vjt9c && mkdir -p /tmp/d20180617-15767-19vjt9c
+mv /home/travis/build/grokking-vietnam/landing/build/.git /tmp/d20180617-15767-19vjt9c/
 cd /home/travis/build/grokking-vietnam/landing
 bundle exec middleman build --clean
-cd -
 mv /tmp/d20180617-15767-19vjt9c/.git /home/travis/build/grokking-vietnam/landing/build
-cd /home/travis/build/grokking-vietnam/landing
-cd -
 cd /home/travis/build/grokking-vietnam/landing/build
 git add --all
 git commit -m "Site updated"
