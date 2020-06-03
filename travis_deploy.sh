38d7d6ebe47b3e61c856250173478d47d1c2cab1
Son Dang <sondangdev@gmail.com>
2020-06-03 23:02:10 +0700

    Move bundler install to before_install in CI

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -9,7 +9,6 @@ git reset --hard origin/gh-pages
 rm -rf /tmp/d20180617-15767-19vjt9c && mkdir -p /tmp/d20180617-15767-19vjt9c
 mv /home/travis/build/grokking-vietnam/landing/build/.git /tmp/d20180617-15767-19vjt9c/
 cd /home/travis/build/grokking-vietnam/landing
-gem install bundler --version '1.16.1'
 bundle exec middleman build --clean
 mv /tmp/d20180617-15767-19vjt9c/.git /home/travis/build/grokking-vietnam/landing/build
 cd /home/travis/build/grokking-vietnam/landing/build
